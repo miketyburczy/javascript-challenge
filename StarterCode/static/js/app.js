35cd15eb15bfd82eb1589400863eb431a86784e0
Mike Tyburczy <miketyburczy@mikes-mbp.lan>
2021-05-08 16:09:15 -0400

    Progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,23 +1,24 @@
 // from data.js
 var tableData = data;
-
 var tbody = d3.select("tbody");
-var dateSelect = d3.select("#datetime")
-var filterButton = d3.select("fitler-btn")
-var resetButton = d3.select("#reset-btn")
 
-function cTable(data) {
+function createTable(data) {
     tbody.html("");
     data.forEach((sighting) => {
         var row = tbody.append("tr")
         Object.entries(sighting).forEach(function([key, value]) {
             console.log(key, value);
-            var addVal = row.append("td")
-            addVal.text(value);
+            var cell = row.append("td");
+            cell.text(value);
         })
     })
 }
 
-function clickButton() {
+var filter = d3.select("#filter-btn");
 
-}
+filter.on("click", function() {
+    d3.select("tbody").html("");
+    d3.preventDefault();
+    var dateTime = d3.select("#datetime").property("value");
+    console.log(datetime);
+})
