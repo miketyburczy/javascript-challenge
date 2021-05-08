a907d64991d0169e325ded4a76df1879c161e6b5
Mike Tyburczy <miketyburczy@mikes-mbp.lan>
2021-05-07 22:42:06 -0400

    Progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var dateSelect = d3.select("#datetime")
+var filterButton = d3.select("fitler-btn")
+var resetButton = d3.select("#reset-btn")
+
+function cTable(data) {
+    tbody.html("");
+    data.forEach((sighting) => {
+        var row = tbody.append("tr")
+        Object.entries(sighting).forEach(function([key, value]) {
+            console.log(key, value);
+            var addVal = row.append("td")
+            addVal.text(value);
+        })
+    })
+}
